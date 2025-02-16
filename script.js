488f56c119ae6266d1313890840456b2d6411e88
jessie-evans <147697556+jessie-evans@users.noreply.github.com>
2025-02-16 19:03:01 +0530

    my personal Portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // Here you can add functionality to send the form data to a server or an email service
+});
